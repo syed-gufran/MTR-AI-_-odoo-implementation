43057afdf9addb948adf8336bc4d7822e4da546f
syed-gufran <tazeema07@gmail.com>
2026-02-20 23:33:45 +0530

    added fields and join reports

--- addons/mtr_module/models/models.py
+++ addons/mtr_module/models/models.py
@@ -404,30 +404,237 @@ class MtrInventoryJoinReport(models.Model):
     _name = "mtr.inventory.join.report"
     _description = "MTR Inventory Join Report"
     _auto = False
-    _rec_name = "lot_number"
+    _rec_name = "inv_lot_number"
 
-    lot_number = fields.Char(readonly=True)
-    heat_number = fields.Char(readonly=True)
-    grade = fields.Char(readonly=True)
-    c_element = fields.Float(string="C", readonly=True)
-    mn_element = fields.Float(string="Mn", readonly=True)
-    yield_strength = fields.Float(readonly=True)
+    join_status = fields.Char(readonly=True)
+
+    inv_entry_no = fields.Integer(readonly=True)
+    inv_date = fields.Date(readonly=True)
+    inv_location_code = fields.Char(readonly=True)
+    inv_item_no = fields.Char(readonly=True)
+    inv_quantity = fields.Float(readonly=True)
+    inv_unit_of_measure_code = fields.Char(readonly=True)
+    inv_document_no = fields.Char(readonly=True)
+    inv_wsi_variant_code = fields.Char(readonly=True)
+    inv_dimensions = fields.Char(readonly=True)
+    inv_lot_number = fields.Char(readonly=True)
+    inv_heat_number = fields.Char(readonly=True)
+    inv_slab_number = fields.Char(readonly=True)
+    inv_internal_bin = fields.Char(readonly=True)
+    inv_additional_notes = fields.Text(readonly=True)
+    inv_cost_amount_actual = fields.Float(readonly=True)
+    inv_description_2 = fields.Char(readonly=True)
+    inv_origin_code = fields.Char(readonly=True)
+    inv_picked = fields.Char(readonly=True)
+    inv_cutting_plan_no = fields.Char(readonly=True)
+    inv_image_path = fields.Char(readonly=True)
+    inv_entry_type = fields.Char(readonly=True)
+    inv_document_type = fields.Char(readonly=True)
+    inv_drawing = fields.Char(readonly=True)
+    inv_yield_value = fields.Float(readonly=True)
+    inv_document_line_no = fields.Integer(readonly=True)
+    inv_revision = fields.Char(readonly=True)
+    inv_laser_quality = fields.Char(readonly=True)
+    inv_unitcost_cwt = fields.Float(readonly=True)
+    inv_piece_no = fields.Char(readonly=True)
+    inv_variant_code = fields.Char(readonly=True)
+    inv_description = fields.Char(readonly=True)
+    inv_return_reason_code = fields.Char(readonly=True)
+    inv_serial_no = fields.Char(readonly=True)
+    inv_package_no = fields.Char(readonly=True)
+    inv_invoiced_quantity = fields.Float(readonly=True)
+    inv_inventory_by_location = fields.Float(readonly=True)
+    inv_inventory = fields.Float(readonly=True)
+    inv_expiration_date = fields.Date(readonly=True)
+    inv_remaining_quantity = fields.Float(readonly=True)
+    inv_shipped_qty_not_returned = fields.Float(readonly=True)
+    inv_reserved_quantity = fields.Float(readonly=True)
+    inv_qty_per_unit_of_measure = fields.Float(readonly=True)
+    inv_sales_amount_expected = fields.Float(readonly=True)
+    inv_sales_amount_actual = fields.Float(readonly=True)
+    inv_cost_amount_expected = fields.Float(readonly=True)
+    inv_cost_amount_non_invtbl = fields.Float(readonly=True)
+    inv_item_description = fields.Char(readonly=True)
+    inv_cost_amount_expected_acy = fields.Float(readonly=True)
+    inv_cost_amount_actual_acy = fields.Float(readonly=True)
+    inv_completely_invoiced = fields.Char(readonly=True)
+    inv_cost_amount_non_invtbl_acy = fields.Float(readonly=True)
+    inv_assemble_to_order = fields.Char(readonly=True)
+    inv_drop_shipment = fields.Char(readonly=True)
+    inv_open_flag = fields.Char(readonly=True)
+    inv_order_type = fields.Char(readonly=True)
+    inv_order_no = fields.Char(readonly=True)
+    inv_order_line_no = fields.Integer(readonly=True)
+    inv_prod_order_comp_line_no = fields.Integer(readonly=True)
+    inv_project_no = fields.Char(readonly=True)
+    inv_project_task_no = fields.Char(readonly=True)
+    inv_source_type = fields.Char(readonly=True)
+    inv_source_no = fields.Char(readonly=True)
+    inv_source_description = fields.Char(readonly=True)
+    inv_source_order_no = fields.Char(readonly=True)
+    inv_grade = fields.Char(readonly=True)
+    inv_weight = fields.Float(readonly=True)
+    inv_posting_date = fields.Date(readonly=True)
+    inv_country_of_melt = fields.Char(readonly=True)
+    inv_country_of_manufacture = fields.Char(readonly=True)
+    inv_source_file = fields.Char(readonly=True)
+
+    mtr_id = fields.Integer(readonly=True)
+    mtr_heat_number = fields.Char(readonly=True)
+    mtr_batch_number = fields.Char(readonly=True)
+    mtr_grade = fields.Char(readonly=True)
+    mtr_manufacturer = fields.Char(readonly=True)
+    mtr_certificate_number = fields.Char(readonly=True)
+    mtr_certificate_date = fields.Date(readonly=True)
+    mtr_c = fields.Float(readonly=True)
+    mtr_mn = fields.Float(readonly=True)
+    mtr_si = fields.Float(readonly=True)
+    mtr_p = fields.Float(readonly=True)
+    mtr_s = fields.Float(readonly=True)
+    mtr_cu = fields.Float(readonly=True)
+    mtr_ni = fields.Float(readonly=True)
+    mtr_cr = fields.Float(readonly=True)
+    mtr_mo = fields.Float(readonly=True)
+    mtr_n = fields.Float(readonly=True)
+    mtr_yield_strength = fields.Float(readonly=True)
+    mtr_tensile_strength = fields.Float(readonly=True)
+    mtr_elongation = fields.Float(readonly=True)
+    mtr_reduction_area = fields.Float(readonly=True)
+    mtr_hardness = fields.Float(readonly=True)
+    mtr_impact_test_temp = fields.Float(readonly=True)
+    mtr_impact_coupon_size = fields.Char(readonly=True)
+    mtr_impact_specimen_1 = fields.Float(readonly=True)
+    mtr_impact_specimen_2 = fields.Float(readonly=True)
+    mtr_impact_specimen_3 = fields.Float(readonly=True)
+    mtr_impact_average = fields.Float(readonly=True)
+    mtr_country_of_melt = fields.Char(readonly=True)
+    mtr_country_of_manufacture = fields.Char(readonly=True)
+    mtr_source_file = fields.Char(readonly=True)
+    mtr_uploaded_at = fields.Datetime(readonly=True)
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute(
             """
             CREATE OR REPLACE VIEW mtr_inventory_join_report AS (
+                WITH latest_mtr AS (
+                    SELECT
+                        m.*,
+                        row_number() OVER (
+                            PARTITION BY m.heat_number
+                            ORDER BY m.uploaded_at DESC NULLS LAST, m.id DESC
+                        ) AS rn
+                    FROM mtr_data m
+                )
                 SELECT
-                    row_number() OVER () AS id,
-                    i.lot_number AS lot_number,
-                    i.heat_number AS heat_number,
-                    i.grade AS grade,
-                    m.c_element AS c_element,
-                    m.mn_element AS mn_element,
-                    m.yield_strength AS yield_strength
+                    i.id AS id,
+                    CASE WHEN m.id IS NULL THEN 'Missing MTR' ELSE 'Matched' END AS join_status,
+                    i.entry_no AS inv_entry_no,
+                    i.date AS inv_date,
+                    i.location_code AS inv_location_code,
+                    i.item_no AS inv_item_no,
+                    i.quantity AS inv_quantity,
+                    i.unit_of_measure_code AS inv_unit_of_measure_code,
+                    i.document_no AS inv_document_no,
+                    i.wsi_variant_code AS inv_wsi_variant_code,
+                    i.dimensions AS inv_dimensions,
+                    i.lot_number AS inv_lot_number,
+                    i.heat_number AS inv_heat_number,
+                    i.slab_number AS inv_slab_number,
+                    i.internal_bin AS inv_internal_bin,
+                    i.additional_notes AS inv_additional_notes,
+                    i.cost_amount_actual AS inv_cost_amount_actual,
+                    i.description_2 AS inv_description_2,
+                    i.origin_code AS inv_origin_code,
+                    i.picked AS inv_picked,
+                    i.cutting_plan_no AS inv_cutting_plan_no,
+                    i.image_path AS inv_image_path,
+                    i.entry_type AS inv_entry_type,
+                    i.document_type AS inv_document_type,
+                    i.drawing AS inv_drawing,
+                    i.yield_value AS inv_yield_value,
+                    i.document_line_no AS inv_document_line_no,
+                    i.revision AS inv_revision,
+                    i.laser_quality AS inv_laser_quality,
+                    i.unitcost_cwt AS inv_unitcost_cwt,
+                    i.piece_no AS inv_piece_no,
+                    i.variant_code AS inv_variant_code,
+                    i.description AS inv_description,
+                    i.return_reason_code AS inv_return_reason_code,
+                    i.serial_no AS inv_serial_no,
+                    i.package_no AS inv_package_no,
+                    i.invoiced_quantity AS inv_invoiced_quantity,
+                    i.inventory_by_location AS inv_inventory_by_location,
+                    i.inventory AS inv_inventory,
+                    i.expiration_date AS inv_expiration_date,
+                    i.remaining_quantity AS inv_remaining_quantity,
+                    i.shipped_qty_not_returned AS inv_shipped_qty_not_returned,
+                    i.reserved_quantity AS inv_reserved_quantity,
+                    i.qty_per_unit_of_measure AS inv_qty_per_unit_of_measure,
+                    i.sales_amount_expected AS inv_sales_amount_expected,
+                    i.sales_amount_actual AS inv_sales_amount_actual,
+                    i.cost_amount_expected AS inv_cost_amount_expected,
+                    i.cost_amount_non_invtbl AS inv_cost_amount_non_invtbl,
+                    i.item_description AS inv_item_description,
+                    i.cost_amount_expected_acy AS inv_cost_amount_expected_acy,
+                    i.cost_amount_actual_acy AS inv_cost_amount_actual_acy,
+                    i.completely_invoiced AS inv_completely_invoiced,
+                    i.cost_amount_non_invtbl_acy AS inv_cost_amount_non_invtbl_acy,
+                    i.assemble_to_order AS inv_assemble_to_order,
+                    i.drop_shipment AS inv_drop_shipment,
+                    i.open_flag AS inv_open_flag,
+                    i.order_type AS inv_order_type,
+                    i.order_no AS inv_order_no,
+                    i.order_line_no AS inv_order_line_no,
+                    i.prod_order_comp_line_no AS inv_prod_order_comp_line_no,
+                    i.project_no AS inv_project_no,
+                    i.project_task_no AS inv_project_task_no,
+                    i.source_type AS inv_source_type,
+                    i.source_no AS inv_source_no,
+                    i.source_description AS inv_source_description,
+                    i.source_order_no AS inv_source_order_no,
+                    i.grade AS inv_grade,
+                    i.weight AS inv_weight,
+                    i.posting_date AS inv_posting_date,
+                    i.country_of_melt AS inv_country_of_melt,
+                    i.country_of_manufacture AS inv_country_of_manufacture,
+                    i.source_file AS inv_source_file,
+                    m.id AS mtr_id,
+                    m.heat_number AS mtr_heat_number,
+                    m.batch_number AS mtr_batch_number,
+                    m.grade AS mtr_grade,
+                    m.manufacturer AS mtr_manufacturer,
+                    m.certificate_number AS mtr_certificate_number,
+                    m.certificate_date AS mtr_certificate_date,
+                    m.c_element AS mtr_c,
+                    m.mn_element AS mtr_mn,
+                    m.si_element AS mtr_si,
+                    m.p_element AS mtr_p,
+                    m.s_element AS mtr_s,
+                    m.cu_element AS mtr_cu,
+                    m.ni_element AS mtr_ni,
+                    m.cr_element AS mtr_cr,
+                    m.mo_element AS mtr_mo,
+                    m.n_element AS mtr_n,
+                    m.yield_strength AS mtr_yield_strength,
+                    m.tensile_strength AS mtr_tensile_strength,
+                    m.elongation AS mtr_elongation,
+                    m.reduction_area AS mtr_reduction_area,
+                    m.hardness AS mtr_hardness,
+                    m.impact_test_temp AS mtr_impact_test_temp,
+                    m.impact_coupon_size AS mtr_impact_coupon_size,
+                    m.impact_specimen_1 AS mtr_impact_specimen_1,
+                    m.impact_specimen_2 AS mtr_impact_specimen_2,
+                    m.impact_specimen_3 AS mtr_impact_specimen_3,
+                    m.impact_average AS mtr_impact_average,
+                    m.country_of_melt AS mtr_country_of_melt,
+                    m.country_of_manufacture AS mtr_country_of_manufacture,
+                    m.source_file AS mtr_source_file,
+                    m.uploaded_at AS mtr_uploaded_at
                 FROM inventory i
-                JOIN mtr_data m ON i.heat_number = m.heat_number
+                LEFT JOIN latest_mtr m
+                    ON i.heat_number = m.heat_number
+                   AND m.rn = 1
             )
             """
         )
